87bbacf4b85337bcb5efff737a9f6b3c29647183
Mohammed Najeeb Mshaweh <urbanrunner13@gmail.com>
2019-05-30 04:19:34 +0200

    initial commit
    created the empty ue4 c++ project, created the gitignore and gitattributes files, synced the repo with my desktop pc

---
+++ IsThisLove/Source/IsThisLove/IsThisLoveGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IsThisLoveGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ISTHISLOVE_API AIsThisLoveGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
