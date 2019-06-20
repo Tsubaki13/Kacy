14d315794fb3d39f47d5ef4331ae0bf7e66d1d6b
Mohammed Najeeb Mshaweh <urbanrunner13@gmail.com>
2019-06-20 21:52:57 +0200

    moved script for the interactable object from blueprint to cpp
    created a Cpp_InteractableObject and made the blueprint inherit from it. now all the script for it is in the cpp and the bp is actually empty with only the static mesh and the collision box in it with no scripting

---
+++ Kacy/Source/Kacy/Actors/Cpp_InteractableItem.cpp
@@ -0,0 +1,31 @@
+// This game was designed and developed by team OnTime as the BA2 semester project for the summer semester 2019 in CologneGameLab in Cologne, Germany. Team OnTime's members: Jann Albrecht (Designer). Patrick Handwerk (Programmer). Mohammed Najeeb Mshaweh (Programmer). Bjoern Roethig (3D Artist). Pauline Mueller (2D Artist).
+
+#include "Cpp_InteractableItem.h"
+
+ACpp_InteractableItem::ACpp_InteractableItem() :
+	bIsInspectable(false)
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ACpp_InteractableItem::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if(bIsInspectable && !(this->Tags.Num() > 0))
+	{
+			this->Tags.Add("Inspectable");
+	}
+	else
+	{
+		if(!bIsInspectable)
+		{
+			this->Tags.Empty();
+		}
+	}
+}
+
+void ACpp_InteractableItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Kacy/Source/Kacy/Actors/Cpp_InteractableItem.h
@@ -0,0 +1,26 @@
+// This game was designed and developed by team OnTime as the BA2 semester project for the summer semester 2019 in CologneGameLab in Cologne, Germany. Team OnTime's members: Jann Albrecht (Designer). Patrick Handwerk (Programmer). Mohammed Najeeb Mshaweh (Programmer). Bjoern Roethig (3D Artist). Pauline Mueller (2D Artist).
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Cpp_InteractableItem.generated.h"
+
+UCLASS()
+class KACY_API ACpp_InteractableItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	ACpp_InteractableItem();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Kacy - Interactable") // this value is set in the blueprint that inherits from this class
+		bool bIsInspectable;
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+};
